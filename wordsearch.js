4b65c6b1a81e19991c33d99bac10d7b3ce40a48a
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-01 19:26:26 -0600

    finished core part

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    const newRow = [];
+    for (const row of matrix) {
+      newRow.push(row[i]);
+    }
+    result.push(newRow);
+  }
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  for (const r of verticalJoin) {
+    if (r.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
